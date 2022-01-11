d155b739078e2adcf69dc940426a3a60eeae2c4c
wooyounggggg <souljit2@gmail.com>
2022-01-11 18:02:41 +0900

    feat: querydsl 설정
    - querydsl 설정을 위한 build.gradle 파일 수정
    - GuestRepository가 QuerydslPredicateExecutor<GuestBook>을 상속하도록 변경

--- src/main/java/com/example/guestbook/repository/GuestRepository.java
+++ src/main/java/com/example/guestbook/repository/GuestRepository.java
@@ -2,7 +2,9 @@ package com.example.guestbook.repository;
 
 import com.example.guestbook.entity.GuestBook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface GuestRepository extends JpaRepository<GuestBook, Long> {
+public interface GuestRepository extends JpaRepository<GuestBook, Long>,
+    QuerydslPredicateExecutor<GuestBook> {
 
 }
