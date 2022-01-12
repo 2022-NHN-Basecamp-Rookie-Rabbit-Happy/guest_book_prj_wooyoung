15a05869b405811069b3dae0de066f55b49aaedb
wooyoung <souljit2@gmail.com>
2022-01-12 16:26:34 +0900

    feat: GuestBookDTO LocalDateTime Format 설정

--- src/main/java/com/example/guestbook/dto/GuestBookDTO.java
+++ src/main/java/com/example/guestbook/dto/GuestBookDTO.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Builder
 @NoArgsConstructor
@@ -16,6 +17,7 @@ public class GuestBookDTO {
     private String title;
     private String content;
     private String writer;
+    @DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss")
     private LocalDateTime regDate, modDate;
 
 }
