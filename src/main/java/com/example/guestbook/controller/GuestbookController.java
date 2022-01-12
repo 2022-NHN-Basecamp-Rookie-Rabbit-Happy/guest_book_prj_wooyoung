c8700337342f82a3fe525c3a728f1e1708d25f3f
wooyoung <souljit2@gmail.com>
2022-01-12 14:03:39 +0900

    feat: GuestBook 등록 API 추가
    - GuestBook 등록 화면 조회 API 추가
    - GuestBook 등록 API 추가

--- src/main/java/com/example/guestbook/controller/GuestbookController.java
+++ src/main/java/com/example/guestbook/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package com.example.guestbook.controller;
 
+import com.example.guestbook.dto.GuestBookDTO;
 import com.example.guestbook.dto.PageRequestDTO;
 import com.example.guestbook.service.GuestBookService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -15,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class GuestbookController {
 
-    private final GuestBookService service;
+    private final GuestBookService guestBookService;
 
     @GetMapping("/")
     public String index() {
@@ -26,7 +29,22 @@ public class GuestbookController {
     public void list(PageRequestDTO pageRequestDTO, Model model) {
         log.info("list........");
 
-        model.addAttribute("result", service.getList(pageRequestDTO));
+        model.addAttribute("result", guestBookService.getList(pageRequestDTO));
     }
 
+    @GetMapping("/register")
+    public void register() {
+        log.info("register get....");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestBookDTO dto, RedirectAttributes redirectAttributes) {
+        log.info("dto..." + dto);
+
+        Long gno = guestBookService.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
+    }
 }
